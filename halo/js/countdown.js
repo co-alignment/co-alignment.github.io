e48d6b7f801d4cb9f6289d7dbbba689219d65340
Co-Alignment <Thecoalignmentlife@gmail.com>
2021-10-25 16:02:34 +0300

    update timer

--- halo/js/countdown.js
+++ halo/js/countdown.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown
-let launchDate = new Date("Oct 21, 2021 17:00:00 UTC").getTime();
+let launchDate = new Date("Oct 26, 2021 18:00:00 UTC").getTime();
 
 // Setup Timer to tick every second
 let timer = setInterval(tick, 1000);
