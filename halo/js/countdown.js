751077f6a45f7796401d2300d0a0df3a77c10696
Co-Alignment <thecoalignmentlife@gmail.com>
2022-03-02 14:42:48 +0300

    Update monthly overview march

--- halo/js/countdown.js
+++ halo/js/countdown.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown
-let launchDate = new Date("FEB 28, 2022 18:00:00 UTC").getTime();
+let launchDate = new Date("MAR 7, 2022 18:00:00 UTC").getTime();
 
 // Setup Timer to tick every second
 let timer = setInterval(tick, 1000);
